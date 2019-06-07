b888f21a3d9663084750912fcef61b3d68ec5592
feoktistov_id <feoktistov.ilya@yandex.ru>
2019-06-07 18:35:37 +0300

    fix header

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -32,15 +32,28 @@
   header.top-header {
     box-shadow: none;
     margin-bottom: 0;
-    background-color: #0074c6 !important;
+    background-color: #0074c6;
   }
   .d-flex {
-    display: flex !important;
+    display: flex;
   }
   .container {
     height: 87px;
+    position: fixed;
+    top: 0;
+    background-color: #0074c6;
   }
   .sub-header {
     height: 312px;
   }
 </style>
+
+<header class="d-flex top-header">
+  <div class="h-100 w-100">
+    <div class="container w-100">
+      <div class="logo">
+      </div>
+    </div>
+    <div class="sub-header w-100" />
+  </div>
+</header>
